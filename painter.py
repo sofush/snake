7801729d9b9730c96d613e5d8f501239f145b1da
sofush <102423577+sofush@users.noreply.github.com>
2023-04-25 02:09:54 +0200

    rename screen to window in painter

--- painter.py
+++ painter.py
@@ -13,11 +13,11 @@ class Painter:
     def __init__(self, board: Board, snake: Snake):
         self.snake = snake
         self.board = board
-        self.screen = pg.display.set_mode((DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT))
+        self.window = pg.display.set_mode((DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT))
         self.tile_size = self.calculate_tile_size()
 
     def clear(self):
-        self.screen.fill(BACKGROUND_COLOR)
+        self.window.fill(BACKGROUND_COLOR)
 
     def paint(self, state: GameState):
         self.clear()
@@ -29,11 +29,11 @@ class Painter:
                     assert tile != None
                     left = MARGIN_PX + (MARGIN_PX * tile.x) + tile.x * self.tile_size[0]
                     top = MARGIN_PX + (MARGIN_PX * tile.y) + tile.y * self.tile_size[1]
-                    rect = Rect((left, top), (self.tile_size))
-                    pg.draw.rect(self.screen, tile.color, rect)
+                    rect = Rect((left, top), self.tile_size)
+                    pg.draw.rect(self.window, tile.color, rect)
 
             score_surface = font.render(f'Score: {self.snake.length() - 1}', True, FONT_COLOR)
-            self.screen.blit(score_surface, (10, 10))
+            self.window.blit(score_surface, (10, 10))
         elif state == GameState.MENU:
             center_width = DEFAULT_SCREEN_WIDTH / 2
             center_height = DEFAULT_SCREEN_HEIGHT / 2
@@ -52,17 +52,17 @@ class Painter:
             instruction_rect = instruction_text.get_rect(center=instruction_pos)
             highscore_rect = highscore_text.get_rect(center=highscore_pos)
 
-            self.screen.blit(instruction_text, instruction_rect)
+            self.window.blit(instruction_text, instruction_rect)
 
             if highscore.date != None:
-                self.screen.blit(highscore_text, highscore_rect)
+                self.window.blit(highscore_text, highscore_rect)
 
     def calculate_tile_size(self) -> (int, int):
         margin_width_total = MARGIN_PX * (self.board.width + 1)
         margin_height_total = MARGIN_PX * (self.board.height + 1)
 
-        width = self.screen.get_width() - margin_width_total
-        height = self.screen.get_height() - margin_height_total
+        width = self.window.get_width() - margin_width_total
+        height = self.window.get_height() - margin_height_total
 
         max_tile_width = int(width / self.board.width)
         max_tile_height = int(height / self.board.height)
